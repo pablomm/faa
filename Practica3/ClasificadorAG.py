22d08487df4bd2edf33d87975d1a4a0fdc04490e
dionivillos <dioni37_rubio@hotmail.com>
2018-11-19 20:42:19 +0100

    empezado practica 3

---
+++ Practica3/ClasificadorAG.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Práctica 3 - Aprendizaje automático
+# Grupo 1463
+# Pareja 10
+# Pablo Marcos Manchón
+# Dionisio Pérez Alvear
+from eps.uam.faa import Practica3
+
+print("Practica 3 realizada. Lo mostramos: ")
+Practica3.RealizaPractica()
+
+#'http://www.sc.ehu.es/ccwbayes/docencia/mmcc/docs/temageneticos.pdf'
